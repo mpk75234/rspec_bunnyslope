71febf4fc39aaf9d6c3baceea222a150ed30214f
mpk75234 <mikekennedy75234@gmail.com>
2020-10-15 14:30:47 -0500

    added attr_accessor which allows memoization of instance created by let

--- BORIS/spec/card_spec.rb
+++ BORIS/spec/card_spec.rb
@@ -1,21 +1,18 @@
 class Card
-  attr_reader :rank, :suit
+  attr_accessor :rank, :suit
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
   end
 end
 RSpec.describe Card do
-
-  before(:example) do
-    puts "I RUN BEFORE!!!"
-  end
-  after(:example) do
-    puts "I RUN AFTER"  
-  end
-  let(:card) {Card.new('Ace', 'Spades')}
-  it 'has a rank' do
+    let(:card) {Card.new('Ace', 'Spades')}
+  it 'has a mutable rank' do
     expect(card.rank).to eq('Ace')
+    card.rank = 'Dongerz'
+    expect(card.rank).to eq('Dongerz')
+  end
+  it 'has a suit' do
     expect(card.suit).to eq('Spades')
   end
 end
