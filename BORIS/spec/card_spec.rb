8f77ea2b4dc2f2c570c7fa3f6f48bd45da19a9c5
mpk75234 <mikekennedy75234@gmail.com>
2020-10-15 13:13:06 -0500

    using let to create instance variable in spec/card_spec.rb

---
+++ BORIS/spec/card_spec.rb
@@ -0,0 +1,15 @@
+class Card
+  attr_reader :rank, :suit
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+end
+RSpec.describe Card do
+
+  let(:card) {Card.new('Ace', 'Spades')}
+  it 'has a rank' do
+    expect(card.rank).to eq('Ace')
+    expect(card.suit).to eq('Spades')
+  end
+end
