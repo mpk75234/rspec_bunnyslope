2b8c834fda433cb34af3e78eb0e04b9cb14172b2
mpk75234 <mikekennedy75234@gmail.com>
2020-10-15 13:24:18 -0500

    added super-bunny-slope before and after hooks

--- BORIS/spec/card_spec.rb
+++ BORIS/spec/card_spec.rb
@@ -7,6 +7,12 @@ class Card
 end
 RSpec.describe Card do
 
+  before(:example) do
+    puts "I RUN BEFORE!!!"
+  end
+  after(:example) do
+    puts "I RUN AFTER"  
+  end
   let(:card) {Card.new('Ace', 'Spades')}
   it 'has a rank' do
     expect(card.rank).to eq('Ace')
