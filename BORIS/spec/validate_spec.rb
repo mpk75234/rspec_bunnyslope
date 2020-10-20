3b910470b094d4883f2b174637b63626c95f64b3
mpk75234 <mikekennedy75234@gmail.com>
2020-10-20 09:25:21 -0500

    added useful validation before after hook

--- BORIS/spec/validate_spec.rb
+++ BORIS/spec/validate_spec.rb
@@ -1,7 +1,30 @@
 #validate UAT is the server.env
+class Server
+  attr_accessor :env,:dc
+  def initialize(env, dc)
+    @env = env
+    @dc = dc
+  end
+end
 
 RSpec.describe 'UAT server validation' do
+  let(:server) { Server.new('COB', 'SWDC')}
+  before do
+    if server.env != 'UAT'
+      puts "ENV MUST BE UAT FOR THIS TEST SUITE...STAPH!!!"
+    else
+      puts "server is #{server.env}"
+    end
+  end
+  after do
+    if server.env != 'UAT'
+      puts "YOU R FUKKING UP THE LAB!!! ENV MUST BE UAT FOR THIS TEST SUITE...STAPH!!!"
+    else
+      puts "server is #{server.env}"
+    end
+  end
+
   it 'should be from MWDC' do
-    expect(server.dc).to eq('MWDC')
+    expect(server.dc).to eq('MWDC'), "Be advised this test suite is for UAT MWDC servers only, not for #{server.env} from #{server.dc}"
   end
 end
