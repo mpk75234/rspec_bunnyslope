00c855ef64c8efba6d09e32879284957c7c6176e
mpk75234 <mikekennedy75234@gmail.com>
2020-11-19 07:41:59 -0600

    card_instance_spec.rb specs all pass

--- BORIS/spec/card_instance_spec.rb
+++ BORIS/spec/card_instance_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Card do
     @card = Card.new('Ace', 'Spades')
   end
   it 'has a rank' do
-    expect(@card.rank).to eq('Eight')
+    expect(@card.rank).to eq('Ace')
   end
   it 'has a suit' do
     expect(@card.suit).to eq('Spades')
